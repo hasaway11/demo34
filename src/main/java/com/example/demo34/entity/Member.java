7a451d8965394f60272c80ed8b524fd461e7c200
Tower <hasaway@naver.com>
2025-10-20 13:41:46 +0900

    2025-10-20 1차

--- src/main/java/com/example/demo34/entity/Member.java
+++ src/main/java/com/example/demo34/entity/Member.java
@@ -1,5 +1,6 @@
 package com.example.demo34.entity;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.*;
@@ -9,8 +10,19 @@ import java.time.*;
 @NoArgsConstructor
 @Builder
 public class Member {
+  // 필수입력. 소문자와 숫자 6~10글자
+  @Pattern(regexp="^[a-z0-9]{6,10}$")
+  @NotEmpty
   private String username;
+
+  // 필수입력. 영숫자 6~10글자
+  @Pattern(regexp="^[A-Za-z0-9]{6,10}$")
+  @NotEmpty
   private String password;
+
+  // 필수입력. 이메일 형식
+  @Email
+  @NotEmpty
   private String email;
   private LocalDate joinDay = LocalDate.now();
   private String role = "USER";
